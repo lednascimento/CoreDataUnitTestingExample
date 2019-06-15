5143eae7b897fce41c86bc261e1699024b3d0ea3
lucabelezal <lucabelezal@gmail.com>
2019-06-14 21:13:43 -0300

    Add tests

--- CoreDataUnitTestingExampleTests/CoreDataUnitTestingExampleTests.swift
+++ CoreDataUnitTestingExampleTests/CoreDataUnitTestingExampleTests.swift
@@ -7,21 +7,77 @@
 //
 
 import XCTest
+import CoreData
 @testable import CoreDataUnitTestingExample
 
 class CoreDataUnitTestingExampleTests: XCTestCase {
 
+    var storageManager: StorageManager?
+
+    var managedObjectModel: NSManagedObjectModel = {
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+        return managedObjectModel
+    }()
+
+    lazy var mockPersistantContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "CoreDataUnitTestingExample", managedObjectModel: self.managedObjectModel)
+        let description = NSPersistentStoreDescription()
+        description.type = NSInMemoryStoreType
+        description.shouldAddStoreAsynchronously = false
+
+        container.persistentStoreDescriptions = [description]
+        container.loadPersistentStores { (description, error) in
+
+            precondition(description.type == NSInMemoryStoreType)
+
+            if let error = error {
+                fatalError("In memory coordinator creation failed \(error)")
+            }
+        }
+        return container
+    }()
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        storageManager = StorageManager(container: mockPersistantContainer)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCheckEmpty() {
+        if let storageManager = storageManager {
+            let rows = storageManager.fetchAll()
+            XCTAssertEqual(rows.count, 0)
+        } else {
+            XCTFail()
+        }
+    }
+
+    func testInsert() {
+
+        guard let storageManager = storageManager else {
+            XCTFail()
+            return
+        }
+
+        let rowsBefore = storageManager.fetchAll()
+        XCTAssertEqual(rowsBefore.count, 0)
+
+        for i in 1...10 {
+            XCTAssertNotNil(storageManager.insertPlace(city: "City \(i)", country: "Country \(i)"))
+        }
+
+        storageManager.save()
+
+        let rowsAfter = storageManager.fetchAll()
+        XCTAssertEqual(rowsAfter.count, 10)
+    }
+
+    func testQuery() {
+        
     }
 
     func testPerformanceExample() {
